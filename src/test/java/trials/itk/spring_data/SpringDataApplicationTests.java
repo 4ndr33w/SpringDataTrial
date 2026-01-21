74825d0a8c26a1aabc66c678d75518bc655e74ae
Andrew Muhlynin <rx.081@hotmail.com>
2026-01-21 19:53:21 +0500

    Initial commit

---
+++ src/test/java/trials/itk/spring_data/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package trials.itk.spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
